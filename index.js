dc18ed56e47fce592ad94ef2a8a62ac29c02c759
Daniel Valmas <daniel.valmas@gmail.com>
2018-09-14 17:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-function init() {
+/*function init() {
   //put any page initialization/handlebars initialization here
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+let recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
+
+function createRecipe(){ 
+  
+  
+} 
+ 
+ 
+let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+let html = template(recipe);
+
+*/
